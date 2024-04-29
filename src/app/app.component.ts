04929cc102bbbcc7dc4d276ecab0f105555d9ffb
Alan.Panicker <alan.panicker@itcinfotech.com>
2024-04-30 00:06:30 +0530

    added some new features

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,24 @@ import { RouterOutlet } from "@angular/router";
 import { CardComponent } from "./components/card/card.component";
 import { HeaderComponent } from "./components/header/header.component";
 import { FooterComponent } from "./components/footer/footer.component";
+import { CommonModule } from "@angular/common";
 
 @Component({
   selector: "app-root",
   standalone: true,
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.css",
-  imports: [RouterOutlet, CardComponent, HeaderComponent, FooterComponent],
+  imports: [
+    RouterOutlet,
+    CardComponent,
+    HeaderComponent,
+    FooterComponent,
+    CommonModule,
+  ],
 })
 export class AppComponent {
-  title = "profile-app";
+  title: string = "profile-app";
+  showProfile: boolean = false;
+  colors: string[] = ["primary", "secondary", "success", "danger"];
+  role: string = "Admin";
 }
