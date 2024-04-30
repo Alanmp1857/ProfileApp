82538cf63e625f25a3fb7a9269dcc0bb58ad7bb4
Alan.Panicker <alan.panicker@itcinfotech.com>
2024-04-30 10:21:47 +0530

    some changes

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,23 +1,24 @@
 import { Component } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
 @Component({
   selector: "app-card",
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: "./card.component.html",
   styleUrl: "./card.component.css",
 })
 export class CardComponent {
-  name = "Alan Mathew Panicker";
+  title: string = "";
 
-  description =
+  name: string = "Alan Mathew Panicker";
+
+  description: string =
     "Highly motivated and adaptable graduate with a strong academic background and a passion for continuous learning. Seeking opportunities to apply theoretical knowledge in practical settings, while contributing to the growth of an innovative organization.";
 
-  location = "Bangalore, India";
+  companyName: string = "ITC Infotech";
 
-  companyName = "ITC Infotech";
-
-  role = "Associate IT Consultant";
+  role: string = "Associate IT Consultant";
 
   openImage() {
     alert("This is a profile image of Alan");
